198047f3a69427d4dc2705f279e37f4baa32579a
Samuel Felipe <samuelfelipef@gmail.com>
2022-08-19 20:56:18 -0300

    initial commit with project setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import ResetStyle from './styles/Reset'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <ResetStyle />
+      <h1>Vittude Login</h1>
+    </>
   );
 }
 
