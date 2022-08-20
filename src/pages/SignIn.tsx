090d9ca589566b8ba638be9c483c093d7f3b0e46
Samuel Felipe <samuelfelipef@gmail.com>
2022-08-20 15:00:26 -0300

    basic sign in page layout aplied

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,7 +1,41 @@
+import * as S from '../styles/SignIn'
+
 const SignIn = () => {
+
+  const submit = () => {
+    return alert('Submit')
+  }
+
   return (
-    <h1>SignIn</h1>
+    <S.Container>
+      <S.Title>Faça seu login</S.Title>
+
+      <S.Form onSubmit={submit} id="signIn">
+        <div>
+          <div>Psicólogo</div>
+          <div>Paciente</div>
+        </div>
+
+        <div>
+          <input
+            required
+            type="email"
+            autoFocus
+          />
+          <input
+            required
+            type="password"
+          />
+        </div>
+      </S.Form>
+
+      <S.Button type="submit" form="signIn">Entrar</S.Button>
+
+      <span>
+        Não é cadastrado? <a href="#" target="_blank">Crie uma conta</a>
+      </span>
+    </S.Container>
   )
 }
 
-export default SignIn
+export default SignIn;
---
+++ src/styles/SignIn.ts
@@ -0,0 +1,32 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  margin-top: 3.75rem;
+  display: flex;
+  flex-direction: column;
+`
+
+export const Title = styled.h1`
+  font-size: 1.75rem;
+  text-align: center;
+  background-color: gray;
+`
+
+export const Form = styled.form`
+  margin-top: 2.25rem;
+  background-color: pink;
+  width: 39.688rem;
+  height: 23.125rem;
+  font-size: 1rem;
+  margin: 0 auto;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+`
+
+export const Button = styled.button`
+  width: 635px;
+  margin: 0 auto;
+`
